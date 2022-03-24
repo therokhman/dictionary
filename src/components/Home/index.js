7ce02af4f022596103cb4e4b98c8ca84d1a056e1
Alexei Rakhmanko <cristiano_ronaldo2000@mail.ru>
2022-03-24 14:25:23 +0300

    add home logic

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,17 @@
 import { Box, Typography, FilledInput } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const [word, setWord] = useState("")
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const trimWord = word.trim();
+        if (trimWord.split(' ').length > 1 || !trimWord) return;
+        navigate(`/search/${word}`)
+    }
     return (
         <Box sx={{
             display: 'flex',
@@ -15,15 +25,21 @@ const Home = () => {
             <Box sx={{
                 width: '500px'
             }}>
-                <FilledInput fullWidth disableUnderline placeholder="Введите слово" startAdornment={<SearchIcon color={"disabled"}/>}
-                         sx={{
-                             backgroundColor: 'white',
-                             borderRadius: 3,
-                             boxShadow: '0px 10px 25px rgba(0, 0, 0, 0.05)',
-                             '& .MuiFilledInput-input': {
-                                 p: 3,
-                             }
-                }}/>
+                <form onSubmit={handleSubmit}>
+                    <FilledInput value={word} onChange={event => setWord(event.target.value)}
+                        fullWidth
+                        disableUnderline
+                        placeholder="Введите слово"
+                        startAdornment={<SearchIcon color={"disabled"}/>}
+                             sx={{
+                                 backgroundColor: 'white',
+                                 borderRadius: 3,
+                                 boxShadow: '0px 10px 25px rgba(0, 0, 0, 0.05)',
+                                 '& .MuiFilledInput-input': {
+                                     p: 3,
+                                 }
+                    }}/>
+                </form>
             </Box>
         </Box>
     )
